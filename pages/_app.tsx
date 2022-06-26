954e67cc472b65c70972c4544b3a43d08bc9f2cf
Lam Tran <lamtran2601@gmail.com>
2022-06-26 18:09:38 +0700

    update css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import 'styles/index.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
